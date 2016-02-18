9a794db41ce3e3d444fa883c054c21be49b0cb56
Aurélien Chatelain <aurelien.chatelain@utbm.fr>
2016-02-18 16:40:46 +0100

    Adds publish Sketchfab files

---
+++ Win/PublishSkfb.h
@@ -0,0 +1,241 @@
+/*
+Copyright (c) 2011, Eric Haines
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "stdafx.h"
+#include <string>
+#include <sstream>
+#include <Windowsx.h>
+#include "Resource.h"
+#include "ExportPrint.h"
+#include "sketchfabUploader.h"
+
+// Sketchfab API field limits
+#define SKFB_NAME_LIMIT         48
+#define SKFB_DESC_LIMIT         1024
+#define SKFB_TAG_LIMIT          29   // 48 but "mineways " and "minecraft " are automatically added so - 19
+#define SKFB_TOKEN_LIMIT        32
+#define SKFB_PASSWORD_LIMIT     64
+
+
+static PublishSkfbData skfbPbdata;
+static std::pair<bool, std::string> lastResponse;
+static HWND uploadWindow;
+static int goPublish;
+
+void getPublishSkfbData(PublishSkfbData *pEpd);
+void setPublishSkfbData(PublishSkfbData *pEpd);
+int uploadToSketchfab(HINSTANCE hInst,HWND hWnd);
+int doPublishSkfb(HINSTANCE hInst,HWND hWnd);
+INT_PTR CALLBACK managePublishWindow(HWND hDlg,UINT message,WPARAM wParam,LPARAM lParam);
+INT_PTR CALLBACK manageUploadWindow(HWND hDlg,UINT message,WPARAM wParam,LPARAM lParam);
+HANDLE uploadThreadHandle = NULL;
+
+
+void getPublishSkfbData(PublishSkfbData *pSkfbpd)
+{
+    *pSkfbpd = skfbPbdata;
+}
+
+void setPublishSkfbData(PublishSkfbData *pSkfbpd)
+{
+    skfbPbdata = *pSkfbpd;
+}
+
+int uploadToSketchfab(HINSTANCE hInst,HWND hWnd)
+{
+    // Success or failure are handled in the callback function
+    DialogBox(hInst,MAKEINTRESOURCE(IDD_UPLOAD_SKFB),hWnd, manageUploadWindow);
+    return 0;
+}
+
+
+int doPublishSkfb(HINSTANCE hInst,HWND hWnd)
+{
+    goPublish = 0;
+    DialogBox(hInst,MAKEINTRESOURCE(IDD_PUBLISH_SKFB),hWnd, managePublishWindow);
+    // did we hit cancel?
+    return goPublish;
+}
+
+DWORD WINAPI thread_func(LPVOID lpParameter)
+{
+    UNREFERENCED_PARAMETER(lpParameter);
+    SketchfabV2Uploader uploader;
+    lastResponse = uploader.upload(uploadWindow, skfbPbdata.skfbApiToken, skfbPbdata.skfbFilePath, skfbPbdata.skfbName, skfbPbdata.skfbDescription, skfbPbdata.skfbTags, skfbPbdata.skfbDraft, skfbPbdata.skfbPrivate, skfbPbdata.skfbPassword);
+    SendMessage(uploadWindow, SIGNAL_UPLOAD_FINISHED, 100, 0);
+    return 0;
+}
+
+INT_PTR CALLBACK manageUploadWindow(HWND hDlg,UINT message,WPARAM wParam,LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        {
+            uploadWindow = hDlg;
+            uploadThreadHandle = CreateThread(NULL, 0, thread_func, NULL, 0, 0);
+            break;
+        }
+    case SIGNAL_UPLOAD_FINISHED:
+    {
+        if(lastResponse.first){ // Upload succeeded
+            int retcode= MessageBox(NULL,
+                _T("Your model has been uploaded. Click OK to view it on Sketchfab"),
+                _T("Upload successful"),
+                MB_OKCANCEL | MB_ICONINFORMATION);
+
+            if(retcode == IDOK)
+            {
+                std::string modelUrl = lastResponse.second;
+                wchar_t* wcharModelUrl = new wchar_t[4096];
+                MultiByteToWideChar(CP_ACP, 0, modelUrl.c_str(), (int)(modelUrl.size() + 1), wcharModelUrl, 4096);
+                ShellExecute(NULL, L"open", wcharModelUrl, NULL, NULL, SW_SHOWNORMAL);
+            }
+        }
+        else { // Upload failed
+            std::vector<wchar_t> errorMessage(MultiByteToWideChar(CP_ACP, 0, lastResponse.second.c_str(), (int)(lastResponse.second.size() + 1), 0, 0));
+            MultiByteToWideChar(CP_ACP, 0, lastResponse.second.c_str(), (int)(lastResponse.second.size() + 1), &errorMessage[0], (int)(errorMessage.size()));
+            std::wstring errorMessageStr(&errorMessage[0]);
+            MessageBox(NULL,
+            errorMessageStr.c_str(),
+                L"Upload failed",
+                MB_OKCANCEL | MB_ICONERROR);
+        }
+
+        EndDialog(hDlg, (INT_PTR)TRUE);
+        return (INT_PTR)TRUE;
+        break;
+    }
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+            case IDC_SKFB_UPLOAD_CANCEL:
+                // Terminate thread
+                TerminateThread(uploadThreadHandle, 1);
+                EndDialog(hDlg, (INT_PTR)FALSE);
+                MessageBox(NULL,
+                    _T("Upload cancelled by the user"), _T("Upload interrupted"), MB_OK | MB_ICONERROR);
+                return (INT_PTR)FALSE;
+            }
+            break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+
+INT_PTR CALLBACK managePublishWindow(HWND hDlg,UINT message,WPARAM wParam,LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        {
+            // to be decoded from UTF-8
+            wchar_t tempName[SKFB_NAME_LIMIT + 1];
+            wchar_t tempPassword[SKFB_PASSWORD_LIMIT + 1];
+
+            // UTF-8 decoding
+            MultiByteToWideChar(CP_UTF8, NULL, skfbPbdata.skfbName, -1, tempName, SKFB_NAME_LIMIT + 1);
+            MultiByteToWideChar(CP_UTF8, NULL, skfbPbdata.skfbPassword, -1, tempPassword, SKFB_PASSWORD_LIMIT + 1);
+
+            SetDlgItemTextA(hDlg,IDC_SKFB_API_TOKEN,skfbPbdata.skfbApiToken);
+            SetDlgItemTextW(hDlg,IDC_SKFB_NAME,tempName);
+            SetDlgItemTextW(hDlg,IDC_SKFB_PASSWORDFIELD,tempPassword);
+
+            CheckDlgButton(hDlg,IDC_SKFB_DRAFT,(skfbPbdata.skfbDraft == false));
+            CheckDlgButton(hDlg,IDC_SKFB_PRIVATE,skfbPbdata.skfbPrivate);
+            Edit_LimitText(GetDlgItem(hDlg, IDC_SKFB_NAME), SKFB_NAME_LIMIT);
+            Edit_LimitText(GetDlgItem(hDlg, IDC_SKFB_API_TOKEN), SKFB_TOKEN_LIMIT);
+            Edit_LimitText(GetDlgItem(hDlg, IDC_SKFB_DESC), SKFB_DESC_LIMIT);
+            Edit_LimitText(GetDlgItem(hDlg, IDC_SKFB_PASSWORDFIELD), SKFB_PASSWORD_LIMIT);
+            EnableWindow(GetDlgItem(hDlg, IDC_SKFB_PASSWORDFIELD), IsDlgButtonChecked(hDlg, IDC_SKFB_PRIVATE));
+        }
+        return (INT_PTR)TRUE;
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+            case IDC_SKFB_CLAIM_TOKEN:
+            {
+                ShellExecute(NULL, L"open", L"http://sketchfab.com/settings/password",
+                    NULL, NULL, SW_SHOWNORMAL);
+                break;
+            }
+            case IDC_SKFB_PRIVATE:
+            {
+                EnableWindow(GetDlgItem(hDlg, IDC_SKFB_PASSWORDFIELD), IsDlgButtonChecked(hDlg, IDC_SKFB_PRIVATE));
+                break;
+            }
+            case ID_SKFB_PUBLISH:
+            {
+                PublishSkfbData lepd = skfbPbdata;
+                // We should not have non UTF8 characters here
+                GetDlgItemTextA(hDlg, IDC_SKFB_API_TOKEN, lepd.skfbApiToken, SKFB_TOKEN_LIMIT + 1);
+
+                // Temporary wide char to be converted to UTF8
+                wchar_t tempName[SKFB_NAME_LIMIT + 1];
+                wchar_t tempDescription[SKFB_DESC_LIMIT + 1];
+                wchar_t tempTags[SKFB_TAG_LIMIT + 1];
+
+                // Get text from controls
+                GetDlgItemTextW(hDlg, IDC_SKFB_NAME, tempName, SKFB_NAME_LIMIT + 1);
+                GetDlgItemTextW(hDlg, IDC_SKFB_DESC, tempDescription, SKFB_DESC_LIMIT + 1);
+                GetDlgItemTextW(hDlg, IDC_SKFB_TAG, tempTags, SKFB_TAG_LIMIT + 1);
+
+                // UTF8 conversion
+                WideCharToMultiByte(CP_UTF8, NULL, tempName, -1, lepd.skfbName, SKFB_NAME_LIMIT + 1, NULL, NULL);
+                WideCharToMultiByte(CP_UTF8, NULL, tempDescription, -1, lepd.skfbDescription, SKFB_DESC_LIMIT + 1, NULL, NULL);
+                WideCharToMultiByte(CP_UTF8, NULL, tempTags, -1, lepd.skfbTags, SKFB_TAG_LIMIT + 1, NULL, NULL);
+
+                if(std::string(lepd.skfbApiToken).size() != 32)
+                {
+                    MessageBox(NULL,
+                        _T("Please check your API token"), _T("Invalid API token"), MB_OK|MB_ICONERROR);
+                    return (INT_PTR)FALSE;
+                }
+                lepd.skfbDraft = (IsDlgButtonChecked(hDlg, IDC_SKFB_DRAFT) == 0);
+                lepd.skfbPrivate = (IsDlgButtonChecked(hDlg, IDC_SKFB_PRIVATE) == 1);
+
+                if (lepd.skfbPrivate){
+                    wchar_t tempPassword[SKFB_PASSWORD_LIMIT + 1];
+                    GetDlgItemTextW(hDlg, IDC_SKFB_PASSWORDFIELD, tempPassword, SKFB_PASSWORD_LIMIT + 1);
+                    WideCharToMultiByte(CP_UTF8, NULL, tempPassword, -1, skfbPbdata.skfbPassword, SKFB_PASSWORD_LIMIT + 1, NULL, NULL);
+                }
+                goPublish = 1;
+
+                // Set it in the main skfbdata object
+                skfbPbdata = lepd;
+            }
+            case IDCANCEL:
+                EndDialog(hDlg, LOWORD(wParam));
+                return (INT_PTR)TRUE;
+            }
+            break;
+    }
+    return (INT_PTR)FALSE;
+}
---
+++ Win/SketchfabUploader.h
@@ -0,0 +1,231 @@
+#include <curl/include/curl.h>
+#include <picojson/picojson.h>
+#include "stdafx.h"
+#include "Resource.h"
+
+#define SKETCHFAB_SERVER "https://sketchfab.com"
+#define SKETCHFAB_MODELS SKETCHFAB_SERVER "/models"
+#define SKETCHFAB_MODELS_API SKETCHFAB_SERVER "/v2/models"
+
+typedef std::map<std::string, std::string> attributes;
+curl_off_t firstStep = -1;
+
+struct ProgressbarUpdater {
+    CURL *curl;
+    HWND progressBar;
+};
+
+// Taken from https://curl.haxx.se/libcurl/c/progressfunc.html
+int progress_callback(void *clientp,
+    curl_off_t dltotal, curl_off_t dlnow,
+    curl_off_t ultotal, curl_off_t ulnow)
+{
+
+    UNREFERENCED_PARAMETER(dltotal);
+    UNREFERENCED_PARAMETER(dlnow);
+    // Hack to smooth the progress bar
+    if(firstStep == -1 && ulnow > 1000)
+        firstStep = ulnow;
+
+    struct ProgressbarUpdater *myp = (struct ProgressbarUpdater *)clientp;
+    double curtime = 0;
+
+    curl_easy_getinfo(myp->curl, CURLINFO_TOTAL_TIME, &curtime);
+
+    curl_off_t total_percen = 0;
+    if(ultotal > CURL_OFF_T_C(1)) {
+        total_percen = (ulnow - firstStep) / ((ultotal - firstStep)/ CURL_OFF_T_C(100));
+        // Hacking the value to get smoother upload progress
+        total_percen = (total_percen - 50) * 2;
+    }
+
+    SendMessage(myp->progressBar, PBM_SETPOS, (WPARAM)float(total_percen), 0);
+
+    return 0;
+};
+
+static size_t WriteMemoryCallback(char *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+class SketchfabV2Uploader {
+public:
+    std::pair<bool, std::string> upload(HWND progressBar,
+                                        const std::string& token,
+                                        const std::string& filepath,
+                                        const std::string& name = std::string(),
+                                        const std::string& description = std::string(),
+                                        const std::string& tags = std::string(),
+                                        const bool draft = false,
+                                        const bool setPrivate = false,
+                                        const std::string& password = std::string())
+    {
+        std::string modelid;
+        std::map<std::string, std::string> parameters, files;
+        HWND pprogressBar = GetDlgItem(progressBar, IDC_SKFB_UPLOAD_PROGRESS);
+
+        // Settings request parameters
+        parameters["token"] = token;
+        parameters["isPublished"] = (draft? "0" : "1");
+        parameters["private"] = (setPrivate ? "1" : "0");
+        parameters["password"] = password;
+        parameters["name"] = name;
+        parameters["tags"] = std::string("mineways ") + std::string("minecraft ") + tags;
+        parameters["description"] = description;
+        parameters["source"] = "mineways";
+        files["modelFile"] = filepath;
+
+        std::pair<int, std::string> response = post(SKETCHFAB_MODELS_API, files, parameters, pprogressBar);
+
+        // Upload v2 returns a status 201 not 200
+        bool success=false;
+        if (response.first == 201)
+          success=true;
+
+        std::string value((success ? model_url(get_json_key(response.second, "uid")) : get_json_key(response.second, "detail")));
+
+        return std::pair<bool, std::string>(success, value);
+    }
+
+
+    std::pair<int, std::string> post(const std::string& url,
+                                     const attributes& files = attributes(),
+                                     const attributes& parameters = attributes(),
+                                     HWND progressBar=NULL)
+    {
+        CURL *curl;
+        struct ProgressbarUpdater prog;
+        CURLcode res;
+        long http_code;
+        std::string response;
+        struct curl_httppost *formpost = NULL;
+        struct curl_httppost *lastptr = NULL;
+        http_code = 0;
+
+        if (url.empty()) {
+            std::cout << "Missing url parameter in post parameters" << std::endl;
+            return std::pair<int, std::string>(-1, std::string());
+        }
+        if (files.empty()) {
+            std::cout << "Missing files in post parameters" << std::endl;
+            return std::pair<int, std::string>(-2, std::string());
+        }
+
+        for (attributes::const_iterator file = files.begin(); file != files.end(); ++file) {
+            std::cout << file->first.c_str();
+			curl_formadd(&formpost,
+				         &lastptr,
+				         CURLFORM_COPYNAME, file->first.c_str(),
+				         CURLFORM_FILE, file->second.c_str(),
+				         CURLFORM_END);
+        }
+
+        for (attributes::const_iterator parameter = parameters.begin(); parameter != parameters.end(); ++parameter) {
+              if (!parameter->second.empty()) {
+                curl_formadd(&formpost,
+                             &lastptr,
+                             CURLFORM_COPYNAME, parameter->first.c_str(),
+                             CURLFORM_COPYCONTENTS, parameter->second.c_str(),
+                             CURLFORM_END);
+              }
+        }
+
+    // Setting curl
+    curl = curl_easy_init();
+
+    if (curl) {
+        prog.curl = curl;
+        prog.progressBar = progressBar;
+
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
+        curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, &prog);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 900L);
+        curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progress_callback);
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            return std::pair<int, std::string>(1, "{\"detail\":\"curl_easy_perform() failed: " + std::string(curl_easy_strerror(res)) + "\" } ");
+        }
+        else {
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+        }
+        SendMessage(progressBar, PBM_SETPOS,100, 0);
+        curl_easy_cleanup(curl);
+        curl_formfree(formpost);
+    }
+
+    return std::pair<int, std::string>(http_code, response);
+}
+
+std::pair<int, std::string> get(const std::string& url,
+                                const attributes& parameters = attributes())
+{
+    CURL *curl;
+    CURLcode res;
+    std::string model_url;
+    std::string response;
+    long http_code;
+
+    if (url.empty()) {
+        std::cout << "Missing url parameters in get parameters" << std::endl;
+        return std::pair<int, std::string>(-1, std::string());
+    }
+
+    std::string options;
+    for (attributes::const_iterator parameter = parameters.begin(); parameter != parameters.end(); ++parameter) {
+        if (options.empty()) {
+            options = "?";
+        }
+        else {
+            options += "&";
+        }
+        options += parameter->first + "=" + parameter->second;
+    }
+
+    http_code = 0;
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, (url + options).c_str());
+        curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 1);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform failed:" << curl_easy_strerror(res) << std::endl;
+            std::cerr << "Model polling returned http code: " << http_code << std::endl;
+        }
+        else {
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    return std::pair<int, std::string>(http_code, response);
+}
+
+std::string model_url(const std::string& modelid) const {
+    return std::string(SKETCHFAB_MODELS) + "/" + modelid;
+}
+
+protected:
+    std::string get_json_key(const std::string& json, const std::string& key) const {
+        picojson::value v;
+        std::string err = picojson::parse(v, json);
+
+        if (v.is<picojson::object>()) {
+            picojson::object obj = v.get<picojson::object>();
+            return obj[key].to_str();
+        }
+        return std::string();
+    }
+};
