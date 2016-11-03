543db0eb26e180b3f36c3b206f76d2d805b37c3a
Eric Haines <erich@acm.org>
2016-11-03 10:12:09 -0400

    Update documentation

--- mineways.bat
+++ mineways.bat
@@ -2,7 +2,9 @@
 
 @rem Starts Mineways, first making the starting window larger and running whatever commands are in the scripting/startup.mwscript file.
 
-@rem These command-line options can be changed: make the window a different size, add more scripts to run, etc.
+@rem These command-line options can be changed: make the window a different size, add more scripts to run, make Mineways look in
+@rem a different directory for your worlds at startup, etc.
+
 @rem See http://mineways.com/scripting.html#clo for more information.
 
 mineways.exe -w 700 700 scripting/startup.mwscript
--- mineways_without_worlds.bat
+++ mineways_without_worlds.bat
@@ -1,5 +1,5 @@
 @rem Run Mineways in a way to avoid loading any worlds stored on your machine.
 @rem Use this .bat file if Mineways "hangs" when you start it.
-@rem All this script does is gives a bogus directory for where your worlds are stored,
+@rem All this script does is gives a directory that exists but (probably) is not where your worlds are stored,
 @rem so that none are loaded.
-mineways.exe -s XXX:\
+mineways.exe -s C:\
