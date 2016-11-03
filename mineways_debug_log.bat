764523cd4492b17100a2f0a2bd28f4b704f52a9f
Eric Haines <erich@acm.org>
2016-11-02 23:08:16 -0400

    new script for tiling, new ways to run Mineways

---
+++ mineways_debug_log.bat
@@ -0,0 +1,4 @@
+@rem Run Mineways and generate a debug logfile of what's going on. This can help you figure out where things
+@rem are going wrong: run Mineways and then look at mineways_exec.log.
+@rem If you're still stumped, contact me, Eric Haines, at http://mineways.com/contact.html
+mineways.exe -l mineways_exec.log
