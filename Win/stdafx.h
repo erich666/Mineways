d82e480ca50a5a783324fd082b383b33b8b509c3
Eric Haines <erich@acm.org>
2019-02-17 11:47:57 -0500

    new version 6.06 labeling

--- Win/stdafx.h
+++ Win/stdafx.h
@@ -67,7 +67,7 @@ THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdio.h>
 
 #define MINEWAYS_MAJOR_VERSION 6
-#define MINEWAYS_MINOR_VERSION 5
+#define MINEWAYS_MINOR_VERSION 6
 
 #ifndef max
 #define max(a,b)            (((a) > (b)) ? (a) : (b))
