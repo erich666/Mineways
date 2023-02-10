d965d98aa3b5b22b494e4468a2e145d08757ac9f
Eric Haines <erich@acm.org>
2023-02-10 13:53:29 -0500

    Fix mwscript registration

--- scripting/register-Mineways-run-as-administrator.bat
+++ scripting/register-Mineways-run-as-administrator.bat
@@ -1,12 +1,10 @@
-@rem You need to run as administrator, e.g., right-click on file and choose this option.
-echo NOTE: this batch file doesn't quite work, I'm not sure why... Help appreciated!
-echo Sets file associations for Mineways to get used when opening Mineways script files.
-echo In other words, when you double-click on an *.mwscript file, Mineways will run.
-echo This is handy for making a script of defaults you like, for when starting up Mineways;
-echo double-click on the script and Mineways will then start with these defaults.
-set program="%~dp0%..\mineways.exe"
+@rem Sets file associations for Mineways to get used when opening Mineways script files.
+@rem Change the path below to wherever your mineways.exe is located.
+@rem You then need to this .bat file with "Run as administrator", e.g., right-click on file and choose this option.
+@rem Setting this up is handy for making a script of defaults you like: double-click on the script
+@rem (such as startup.mwscript) and Mineways will then start with these defaults.
 
 assoc .mwscript=Mineways
-ftype Mineways=..\%program% "%%1"
+ftype Mineways=C:\Users\erich\Downloads\mineways1009\mineways\mineways.exe "%%1"
 
 pause
