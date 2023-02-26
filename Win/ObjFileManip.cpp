5075c49dc98dcb62db3bf18f16ab6ccecb3357b5
Eric Haines <erich@acm.org>
2023-02-26 09:42:28 -0500

    1.7% decrease by corner neighbor test

--- Win/ObjFileManip.cpp
+++ Win/ObjFileManip.cpp
@@ -32143,6 +32143,35 @@ static void mergeSimplifySet(SimplifyFaceRecord** ppSFR, int faceCount)
             // for extending in Y, we need the current record in the lower left
             SimplifyFaceRecord* pLowerLeftSFR = pCornerSFR;
 
+            // quick out: if there's a neighbor in X but not Y, or vice versa, and there's a diagonally
+            // neighbor, then ignore this one and try to get a 2xN tile next time around.
+            SimplifyFaceRecord* pNextX = pCornerSFR->pXneighborSFR;
+            bool hasX = (pNextX != NULL && pNextX->pFace != NULL);
+            SimplifyFaceRecord* pNextY = pCornerSFR->pYneighborSFR;
+            bool hasY = (pNextY != NULL && pNextY->pFace != NULL);
+            if (!hasX && !hasY) {
+                // quick out
+                continue;
+            }
+            if (hasX != hasY) {
+                // has one neighbor, not the other. Does diagonal neighbor exist? If so,
+                // then don't search this one
+                if (hasX) {
+                    if (pNextX->pYneighborSFR != NULL && pNextX->pYneighborSFR->pFace != NULL) {
+                        // has neighbor
+                        continue;
+                    }
+                }
+                else {
+                    // hasY is true
+                    if (pNextY->pXneighborSFR != NULL && pNextY->pXneighborSFR->pFace != NULL) {
+                        // has neighbor
+                        continue;
+                    }
+                }
+            }
+
+            // now walk in X and Y directions, alternating, growing the rectangle
             do {
                 if (testX) {
                     // Walk through all records in current column, see if all have X+1 neighbors.
