e8bab46685dbbd3e80a03da02a171418b1f7d34c
Eric Haines <erich@acm.org>
2024-01-04 17:27:12 -0500

    V11.07 number update

--- Win/stdafx.h
+++ Win/stdafx.h
@@ -67,7 +67,7 @@ THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdio.h>
 
 #define MINEWAYS_MAJOR_VERSION 11
-#define MINEWAYS_MINOR_VERSION 06
+#define MINEWAYS_MINOR_VERSION 07
 
 #ifndef max
 #define max(a,b)            (((a) > (b)) ? (a) : (b))
